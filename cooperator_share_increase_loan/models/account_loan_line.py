6943b2dad443f2090c508f5cab93abbbed93942a
Nicolás Ramos <nicolas.ramos@somit.coop>
2024-11-11 14:39:17 +0100

    Update account_loan_line.py

--- cooperator_share_increase_loan/models/account_loan_line.py
+++ cooperator_share_increase_loan/models/account_loan_line.py
@@ -9,68 +9,38 @@ class AccountLoanLine(models.Model):
         vals = []
 
         if self.loan_id.loan_type == "interest" and self.loan_id.from_subscription:
-
             interest_account = self.loan_id.interest_expenses_account_id
-
             taxes = interest_account.tax_ids
             if taxes:
-                tax_values = {
-                    "amount_currency": self.interests_amount,
-                    "currency_id": self.loan_id.company_id.currency_id.id,
-                    "tax_ids": [Command.set(taxes.ids)],
-                    "partner_id": self.loan_id.partner_id.id,
-                    "account_id": interest_account.id,
-                }
-
                 tax_results = taxes.compute_all(
                     self.interests_amount,
-                    currency=self.loan_id.company_id.currency_id,
+                    currency=self.loan_id.currency_id,
+                    quantity=1.0,
+                    product=None,
                     partner=self.loan_id.partner_id,
                 )
 
-                vals.append(
-                    {
-                        "account_id": interest_account.id,
-                        "credit": 0,
-                        "debit": tax_results["total_included"],
-                        "tax_ids": [],
-                    }
-                )
-
-                for tax_vals in tax_results["taxes"]:
-                    if tax_vals["amount"]:
-                        tax = self.env["account.tax"].browse(tax_vals["id"])
-                        vals.append(
-                            {
-                                "account_id": tax_vals["account_id"],
-                                "credit": (
-                                    tax_vals["amount"] if tax_vals["amount"] > 0 else 0
-                                ),
-                                "debit": (
-                                    -tax_vals["amount"] if tax_vals["amount"] < 0 else 0
-                                ),
-                                "tax_line_id": tax_vals["id"],
-                                "name": tax.name,
-                            }
-                        )
-            else:
-                vals.append(
-                    {
-                        "account_id": interest_account.id,
-                        "credit": 0,
-                        "debit": self.interests_amount,
-                    }
-                )
-
-            vals.append(
-                {
-                    "account_id": self.loan_id.partner_id.property_account_payable_id.id,
-                    "partner_id": self.loan_id.partner_id.id,
-                    "credit": self.payment_amount,
-                    "debit": 0,
+                tax_values = {
+                    "name": "Loan interest",
+                    "amount_currency": self.interests_amount,
+                    "currency_id": self.loan_id.company_id.currency_id.id,
+                    "tax_ids": [Command.set(taxes.ids)],
+                    "account_id": interest_account.id,
+                    "debit": tax_results["total_included"],
+                    "credit": 0.0,
                 }
-            )
+                vals.append(tax_values)
 
-            return vals
-        else:
-            return super()._move_line_vals(account=account)
+                partner = self.loan_id.partner_id.with_company(self.loan_id.company_id)
+                balance_line = {
+                    "name": "Interest payment",
+                    "amount_currency": -tax_results["total_included"],
+                    "currency_id": self.loan_id.company_id.currency_id.id,
+                    "partner_id": self.loan_id.partner_id.id,
+                    "account_id": partner.property_account_payable_id.id,
+                    "debit": 0.0,
+                    "credit": tax_results["total_included"],
+                }
+                vals.append(balance_line)
+
+        return vals
